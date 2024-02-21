e2f26f0f2def18ab8887b9e7befffe7645d164f7
bmapz <bmaphosa01@gmail.com>
2024-02-20 23:43:55 -0500

    Update HelloWorldTest.java

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -39,5 +39,50 @@ class HelloWorldTest {
 	    HelloWorld.main(null);
 		assertEquals("Please input your name: \nHello, Prakash", out.toString().strip());
 	}
+	import static org.junit.jupiter.api.Assertions.*;
+
+	import java.io.ByteArrayOutputStream;
+	import java.io.IOException;
+	import java.io.ByteArrayInputStream;
+	import java.io.PrintStream;
+	import java.io.InputStream;
+
+	import org.junit.jupiter.api.AfterEach;
+	import org.junit.jupiter.api.BeforeEach;
+	import org.junit.jupiter.api.Test;
+
+	class HelloWorldTest {
+		a
+		private static final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		private static final ByteArrayOutputStream err = new ByteArrayOutputStream();
+		private static final PrintStream originalOut = System.out;
+		private static final PrintStream originalErr = System.err;
+		private static final InputStream originalIn = System.in;
+
+		@BeforeEach
+		void setUp() throws Exception {
+		    System.setOut(new PrintStream(out));
+		    System.setErr(new PrintStream(err));
+		}
+
+		@AfterEach
+		void tearDown() throws Exception {
+		    System.setOut(originalOut);
+		    System.setErr(originalErr);
+		    System.setIn(originalIn);
+		}
+
+		@Test
+		void test() throws IOException {
+			byte[] initialArray = "Prakash\n".getBytes();
+		    InputStream targetStream = new ByteArrayInputStream(initialArray);
+			System.setIn(targetStream);
+		    HelloWorld.main(null);
+		    String actualOutput = out.toString().strip();
+		    actualOutput = actualOutput.replaceAll("[\\n\\r]", "");
+			assertEquals("Please input your name: Hello, Prakash", actualOutput);
+		}
+
+	}
 
 }
